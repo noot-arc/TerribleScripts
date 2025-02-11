1c8c502aaca3ecf70a794907cea1770dd640e3fa
noot-arc <noot@skibidisoftware>
2025-02-11 22:15:53 +0300

    rough script for bdcc calculation

---
+++ src/CalculateBDCC/CalculateBDCC.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using FistVR;
+using UnityEngine;
+
+namespace nootarc
+{
+    public partial class ScaleBDCC : ScriptableObject
+    {
+        [ContextMenu("CalculateBDCC")]
+        public void ScaleCurve(AnimationCurve curve, float maxX, float maxY)
+        {
+            for (int i = 0; i < curve.keys.Length; i++)
+            {
+                Keyframe keyframe = curve.keys[i];
+                keyframe.value = curve.keys[i].value * maxY;
+                keyframe.time = curve.keys[i].time * maxX;
+                keyframe.inTangent = curve.keys[i].inTangent * maxY / maxX;
+                keyframe.outTangent = curve.keys[i].outTangent * maxY / maxX;
+
+                scaledCurve.AddKey(keyframe);
+            }
+            this.scaledCurve = new AnimationCurve();
+        }
+        public AnimationCurve curve;
+        public AnimationCurve scaledCurve;
+        public float maxX = 1f;
+        public float maxY = 0.1f;
+    }
+}
