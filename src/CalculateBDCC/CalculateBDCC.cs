98719067f726e3f8f5b870895ffb135ee9bc769c
noot-arc <noot@skibidisoftware>
2025-02-12 07:56:34 +0300

    BDCC script works this time! I hope...

--- src/CalculateBDCC/CalculateBDCC.cs
+++ src/CalculateBDCC/CalculateBDCC.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 namespace nootarc
 {
+    [CreateAssetMenu(menuName = "Misc/CalculateBDCC", fileName = "NewBDCCurve")]
     public partial class ScaleBDCC : ScriptableObject
     {
-        [ContextMenu("CalculateBDCC")]
-        public void ScaleCurve(AnimationCurve curve, float maxX, float maxY)
+        [ContextMenu("Scale Curve")]
+        public void ScaleCurve()
         {
-            for (int i = 0; i < curve.keys.Length; i++)
             {
-                Keyframe keyframe = curve.keys[i];
-                keyframe.value = curve.keys[i].value * maxY;
-                keyframe.time = curve.keys[i].time * maxX;
-                keyframe.inTangent = curve.keys[i].inTangent * maxY / maxX;
-                keyframe.outTangent = curve.keys[i].outTangent * maxY / maxX;
+                for (int i = 0; i < curve.keys.Length; i++)
+                {
+                    Keyframe keyframe = curve.keys[i];
+                    keyframe.value = curve.keys[i].value * Height;
+                    keyframe.time = curve.keys[i].time * Width;
+                    keyframe.inTangent = curve.keys[i].inTangent * Height / Width;
+                    keyframe.outTangent = curve.keys[i].outTangent * Height / Width;
 
-                scaledCurve.AddKey(keyframe);
+                    scaledCurve.AddKey(keyframe);
+                }
+                return;
             }
-            this.scaledCurve = new AnimationCurve();
         }
         public AnimationCurve curve;
         public AnimationCurve scaledCurve;
-        public float maxX = 1f;
-        public float maxY = 0.1f;
+        public float Width = 1f;
+        public float Height = 0.1f;
     }
 }
