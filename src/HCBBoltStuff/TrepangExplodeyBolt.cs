0b3ef05f4262c372c96626b7b19b13e7eebe4a49
noot-arc <noot@skibidisoftware>
2025-02-20 18:33:16 +0300

    feature 2: actually blow up. also do the sound and particle effect things.

--- src/HCBBoltStuff/TrepangExplodeyBolt.cs
+++ src/HCBBoltStuff/TrepangExplodeyBolt.cs
@@ -6,13 +6,31 @@ namespace nootarc.HCBBoltStuff;
 
 public class TrepangExplodeyBolt : MonoBehaviour
 {
-    private float fuseTime = 3f;
-    private List<GameObject> ExplosionSpawns;
+    public ParticleSystem fuseParticles;
+    public HCBBolt HCBBolt;
+    public AudioEvent fuseSound;
+    public float fuseTime = 3f;
+    public float startFuseTime = 3f;
+    public List<GameObject> ExplosionSpawns;
     public int IFF;
+    public float fuseTick;
     
-    public void FVRUpdate()
+    public void Update()
     {
         fuseTime -= Time.deltaTime;
+        float num2 = Mathf.Clamp(1f - fuseTime / startFuseTime, 0f, 1f);
+        num2 = Mathf.Pow(num2, 2f);
+        if (fuseTick <= 0f)
+        {
+            fuseTick = Mathf.Lerp(0.4f, 0.02f, num2);
+            float num3 = Mathf.Lerp(1.8f, 3.7f, num2);
+            SM.PlayCoreSoundOverrides(FVRPooledAudioType.Generic, fuseSound, transform.position, new Vector2(1f, 1f), new Vector2(num3, num3));
+            fuseParticles.Emit(2);
+        }
+        else
+        {
+            fuseTick -= Time.deltaTime;
+        }
         if (fuseTime <= 0)
         {
             foreach (var t in ExplosionSpawns)
